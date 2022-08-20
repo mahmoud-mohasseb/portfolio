a5ce391e25f495e5bbbdcb895dce597dcd631344
Mac X <ghareb4@gmail.com>
2022-08-20 15:18:57 +0300

    changes done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
             Get In Touch
           </h4>
           <p className="text-sub font-medium md:w-1/2 mx-auto">
-            Although I’m not currently looking for any new opportunities, my
+            Although I’m currently looking for any new opportunities, my
             inbox is always open. Whether you have a question or just want to
             say hi, I’ll try my best to get back to you!
           </p>
